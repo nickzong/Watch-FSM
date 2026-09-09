c91d984afad88f31a5810c34e62a7b687c37884a
Nicholas Zong <nzong@Nicholass-MacBook-Pro.local>
2026-09-08 17:17:10 -0700

    added blinking hh/mm during set_hr/min modes for clock and alarm, most logic now depends on button pulses instead of levels to avoid possible problems if buttons are held down

--- watch.sv
+++ watch.sv
@@ -7,6 +7,7 @@
 module watch (
     input logic clk,
     input logic tick_1Hz,
+    input logic blink_en, // 2 Hz signal to drive blinking in set modes
     input logic b_mode,
     input logic b_set,
     input logic b_plus,
@@ -16,22 +17,36 @@ module watch (
 );
 
     logic[1:0] state, next_state;
-    logic[1:0] c_state = 2'b00; // tracker for the current clock state
-    logic[1:0] a_state = 2'b00; // tracker for the current alarm state
-
-    // signals for watch mode instance inputs
-    logic fact_rst; // when in time state, if all three buttons are pressed at once, factory reset everything
-    logic trigger_alarm = 0;
-    logic true_mode, true_set, true_plus;
+    logic[1:0] c_state; // tracker for the current clock state
+    logic[1:0] a_state; // tracker for the current alarm state
+    logic s_carry;
 
     // internal registers used to store time for all three modes
-    logic[5:0] time_ss = 0; 
-    logic[5:0] time_mm = 0;
-    logic[4:0] time_hh = 0;
-    logic[5:0] sw_ss = 0;
-    logic[5:0] sw_mm = 0;
-    logic[5:0] alarm_mm = 0;
-    logic[4:0] alarm_hh = 0;
+    logic[5:0] time_ss; 
+    logic[5:0] time_mm;
+    logic[4:0] time_hh;
+    logic[5:0] sw_ss;
+    logic[5:0] sw_mm;
+    logic[5:0] alarm_mm;
+    logic[4:0] alarm_hh;
+
+    // signals for watch mode instances' inputs
+    logic fact_rst; // when in time state, if all three buttons are pressed at once, factory reset everything
+    logic trigger_alarm;
+    logic true_mode, true_set, true_plus; // encodes AMSP hierarchy
+
+    // button pulses that stay at 1 for only 1 clk cycle
+    logic b_mode_prev, mode_pulse;
+    always_ff @(posedge clk) b_mode_prev <= b_mode;
+    assign mode_pulse = b_mode & ~b_mode_prev; // one cycle wide exactly on the rising edge
+
+    logic b_set_prev, set_pulse;
+    always_ff @(posedge clk) b_set_prev <= b_set;
+    assign set_pulse = b_set & ~b_set_prev; // one cycle wide, exactly on the rising edge
+
+    logic b_plus_prev, plus_pulse;
+    always_ff @(posedge clk) b_plus_prev <= b_plus;
+    assign plus_pulse = b_plus & ~b_plus_prev; // one cycle wide, exactly on the rising edge
 
     clock u_clock ( 
         .clk(clk),
@@ -39,7 +54,6 @@ module watch (
         .tick_1Hz(tick_1Hz),
         .plus(true_plus && (state == 2'b00)),
         .set(true_set && (state == 2'b01)),
-        .pause_sec(pause_sec),
         .s_carry(s_carry),
         .state(c_state),
         .ss(time_ss),
@@ -71,14 +85,14 @@ module watch (
     );
     
     // true signals go to 1 if the signal is the highest priority in the AMSP hierarchy: async alarm > mode > set > plus
-    assign true_mode = !trigger_alarm & b_mode;
-    assign true_set = !trigger_alarm & !b_mode & b_set;
-    assign true_plus = !trigger_alarm & !b_mode & !b_set & b_plus;
+    assign true_mode = !trigger_alarm & mode_pulse;
+    assign true_set = !trigger_alarm & !mode_pulse & set_pulse;
+    assign true_plus = !trigger_alarm & !mode_pulse & !set_pulse & plus_pulse;
 
-    always_comb @(*) begin
-        if (trigger_alarm) begin 
-            next_state = 2'b11;
-        end 
+    always_comb begin
+        next_state = state; // default
+        if (trigger_alarm)  next_state = 2'b11; 
+
         // mode button and alarm silence functions
         // define mode cycle: time (00) --> stopwatch (01) --> alarm (10) --> time (00)
         case (state)
@@ -86,39 +100,72 @@ module watch (
             2'b01: if (true_mode) next_state = 2'b10;
             2'b10: if (true_mode && a_state == 2'b00) next_state = 2'b00; // can only switch if idle
             2'b11: if (true_mode || true_set || true_plus) next_state = 2'b00; // silence alarm, alarm_ring state
-            default: next_state = 2'b00;
+            default: next_state = state;
         endcase
         
-        //TODO add a combo logic block to control the display flow
+        // display output logic
         case (state)
             2'b00: begin
-                disp_pair1 = time_hh;
-                disp_pair2 = time_mm;
+                case (c_state) 
+                    2'b00: begin // idle
+                        disp_pair1 = time_hh;
+                        disp_pair2 = time_mm;
+                    end
+                    2'b01: begin // set_hr
+                        disp_pair1 = (blink_en) ? time_hh : '0;
+                        disp_pair2 = time_mm;
+                    end
+                    2'b10: begin // set_min
+                        disp_pair1 = time_hh;
+                        disp_pair2 = (blink_en) ? time_mm : '0;
+                    end
+                    default: begin
+                        disp_pair1 = time_hh;
+                        disp_pair2 = time_mm;
+                    end
+                endcase
             end
             2'b01: begin
                 disp_pair1 = sw_mm;
                 disp_pair2 = sw_ss;
             end
             2'b10: begin
-                disp_pair1 = alarm_hh;
-                disp_pair2 = alarm_mm;
+                case (a_state) 
+                    2'b00: begin // idle
+                        disp_pair1 = alarm_hh;
+                        disp_pair2 = alarm_mm;
+                    end
+                    2'b01: begin // set_hr
+                        disp_pair1 = (blink_en) ? alarm_hh : '0;
+                        disp_pair2 = alarm_mm;
+                    end
+                    2'b10: begin // set_min
+                        disp_pair1 = alarm_hh;
+                        disp_pair2 = (blink_en) ? alarm_mm : '0;
+                    end
+                    default: begin
+                        disp_pair1 = alarm_hh;
+                        disp_pair2 = alarm_mm;
+                    end
+                endcase
             end
             2'b11: begin
-                disp_pair1 = time_hh;
-                disp_pair2 = time_mm; // TODO fill in with actual alarm 
+                disp_pair1 = (blink_en) ? time_hh : '0;
+                disp_pair2 = (blink_en) ? time_mm : '0;
             end
             default:  begin
                 disp_pair1 = time_hh;
                 disp_pair2 = time_mm;
             end
         endcase
+
     end
 
     always_ff @(posedge clk, posedge fact_rst) begin
-        if (fact_rst) state = 2'b00;
-        else state = next_state;
+        if (fact_rst) state <= 2'b00;
+        else state <= next_state;
     end
     
-    assign fact_rst = true_mode & true_set & true_plus;
+    assign fact_rst = b_mode & b_set & b_plus;
     
 endmodule
