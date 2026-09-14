f090909977076d4911f020462232f34b8af5bb88
Nicholas Zong <nzong@Nicholass-MacBook-Pro.local>
2026-09-14 16:49:54 -0700

    updated blink signal to be derived from clk

--- watch.sv
+++ watch.sv
@@ -1,7 +1,6 @@
 module watch (
     input logic clk,
     input logic tick_1Hz,
-    input logic blink_en, // 2 Hz signal to drive blinking in set modes
     input logic b_mode_raw,
     input logic b_set_raw,
     input logic b_plus_raw,
@@ -32,6 +31,7 @@ module watch (
     // signals for watch mode instances' inputs
     logic fact_rst; // when in time state, if all three buttons are pressed at once, factory reset everything
     logic true_mode, true_set, true_plus; // encodes AMSP hierarchy
+    logic blink_en; // 2 Hz signal to drive blinking in set modes
 
     // ------ PROCESS BUTTON INPUTS ------
     logic b_mode, b_mode_prev, mode_pulse;
@@ -115,6 +115,11 @@ module watch (
         .top_ones_7sd(top_ones_7sd),
         .bottom_tens_7sd(bottom_tens_7sd),
         .bottom_ones_7sd(bottom_ones_7sd)
+    );
+
+    blink_signal u_blink (
+        .clk(clk),
+        .blink_en(blink_en)
     );
 
     always_comb begin
